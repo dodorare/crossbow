66340f59eeba35ca628b9207ec702c625b7e1676
enfipy <enfipy@gmail.com>
2020-11-28 20:03:10 +0200

    Remove toml and alter error

--- crates/cargo-creator/src/error.rs
+++ crates/cargo-creator/src/error.rs
@@ -3,7 +3,6 @@ use cargo_toml::Error as CargoTomlError;
 use displaydoc::Display;
 use std::io::Error as IoError;
 use thiserror::Error;
-use toml::de::Error as TomlError;
 
 #[derive(Display, Debug, Error)]
 pub enum Error {
@@ -14,18 +13,9 @@ pub enum Error {
     /// Didn't find rustc
     RustcNotFound,
     /// Failed to parse config: {0}
-    Config(#[from] TomlError),
+    Config(#[from] CargoTomlError),
     /// IO error: {0}
     Io(#[from] IoError),
     /// NDK error: {0}
     Ndk(#[from] NdkError),
-}
-
-impl From<CargoTomlError> for Error {
-    fn from(error: CargoTomlError) -> Error {
-        match error {
-            CargoTomlError::Io(io) => io.into(),
-            CargoTomlError::Parse(toml) => toml.into(),
-        }
-    }
 }
