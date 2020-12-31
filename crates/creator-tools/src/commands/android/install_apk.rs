36e96d13d6112f919ff794ba6e95a532a42a0a1c
olvyko <olvyko@gmail.com>
2020-12-31 17:54:25 +0600

    Add install apk command

---
+++ crates/creator-tools/src/commands/android/install_apk.rs
@@ -0,0 +1,10 @@
+use crate::deps::AndroidSdk;
+use crate::error::*;
+use std::path::Path;
+
+pub fn install_apk(sdk: &AndroidSdk, apk_path: &Path) -> Result<()> {
+    let mut adb = sdk.platform_tool(bin!("adb"))?;
+    adb.arg("install").arg("-r").arg(apk_path);
+    adb.output_err()?;
+    Ok(())
+}
--- crates/creator-tools/src/commands/android/mod.rs
+++ crates/creator-tools/src/commands/android/mod.rs
@@ -3,6 +3,7 @@ mod align_apk;
 mod gen_debug_key;
 mod gen_manifest;
 mod gen_unaligned_apk;
+mod install_apk;
 mod rust_compile;
 mod sign_apk;
 
@@ -11,5 +12,6 @@ pub use align_apk::*;
 pub use gen_debug_key::*;
 pub use gen_manifest::*;
 pub use gen_unaligned_apk::*;
+pub use install_apk::*;
 pub use rust_compile::*;
 pub use sign_apk::*;
