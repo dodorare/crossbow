8565795c608b9dfa48d43d93a537ec5b1ddb15b8
Adoka3710 <adicoolwest@gmail.com>
2021-07-19 13:33:19 +0600

    Add impl Bundletool

--- crates/creator-tools/src/tools/bundletool/mod.rs
+++ crates/creator-tools/src/tools/bundletool/mod.rs
@@ -5,3 +5,22 @@ mod get_size_total;
 pub use build_apks::*;
 pub use build_bundle::*;
 pub use get_size_total::*;
+
+use std::path::{Path, PathBuf};
+
+#[derive(Clone, Copy)]
+pub struct Bundletool;
+
+impl Bundletool {
+    pub fn build_apks(self, bundle: &Path, output: &Path) -> BuildApks {
+        BuildApks::new(bundle, output)
+    }
+
+    pub fn build_bundle(self, modules: &[PathBuf], output: &Path) -> BuildBundle {
+        BuildBundle::new(modules, output)
+    }
+
+    pub fn get_size_total(self, apks: &Path) -> GetSizeTotal {
+        GetSizeTotal::new(apks)
+    }
+}
