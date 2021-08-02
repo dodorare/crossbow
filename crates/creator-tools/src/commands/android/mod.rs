f3f6285513f1a50194cceaf154cd680ad04efabf
OffTheHeezay <den_9912@mail.ru>
2021-08-02 15:30:07 +0300

    Add write_zip

--- crates/creator-tools/src/commands/android/mod.rs
+++ crates/creator-tools/src/commands/android/mod.rs
@@ -4,6 +4,7 @@
 mod add_libs_into_apk;
 mod align_apk;
 mod detect_abi;
+mod extract_apk;
 mod gen_debug_key;
 mod gen_manifest;
 mod gen_unaligned_apk;
@@ -14,11 +15,12 @@ mod rust_compile;
 mod save_manifest;
 mod sign_apk;
 mod start_apk;
-mod extract_apk;
+mod write_zip;
 
 pub use add_libs_into_apk::*;
 pub use align_apk::*;
 pub use detect_abi::*;
+pub use extract_apk::*;
 pub use gen_debug_key::*;
 pub use gen_manifest::*;
 pub use gen_unaligned_apk::*;
@@ -29,4 +31,4 @@ pub use rust_compile::*;
 pub use save_manifest::*;
 pub use sign_apk::*;
 pub use start_apk::*;
-pub use extract_apk::*;
+pub use write_zip::*;
---
+++ crates/creator-tools/src/commands/android/write_zip.rs
@@ -0,0 +1,74 @@
+use std::{
+    fs::File,
+    io::{Read, Seek, Write},
+    iter::Iterator,
+    path::Path,
+};
+use walkdir::{DirEntry, WalkDir};
+use zip::{result::ZipError, write::FileOptions};
+
+fn zip_dir<T>(
+    it: &mut dyn Iterator<Item = DirEntry>,
+    prefix: &str,
+    writer: T,
+    method: zip::CompressionMethod,
+) -> zip::result::ZipResult<()>
+where
+    T: Write + Seek,
+{
+    let mut zip = zip::ZipWriter::new(writer);
+    let options = FileOptions::default().compression_method(method);
+
+    let mut buffer = Vec::new();
+    for entry in it {
+        let path = entry.path();
+        let name = path.strip_prefix(Path::new(prefix)).unwrap();
+
+        if path.is_file() {
+            println!("adding file {:?} as {:?} ...", path, name);
+            #[allow(deprecated)]
+            zip.start_file_from_path(name, options)?;
+            let mut f = File::open(path)?;
+
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+            buffer.clear();
+        } else if name.as_os_str().len() != 0 {
+            println!("adding dir {:?} as {:?} ...", path, name);
+            #[allow(deprecated)]
+            zip.add_directory_from_path(name, options)?;
+        }
+    }
+    zip.finish()?;
+    Result::Ok(())
+}
+
+fn write_zip(
+    src_dir: &str,
+    dst_file: &str,
+    method: zip::CompressionMethod,
+) -> zip::result::ZipResult<()> {
+    if !Path::new(src_dir).is_dir() {
+        return Err(ZipError::FileNotFound);
+    }
+
+    let path = Path::new(dst_file);
+    let file = File::create(&path).unwrap();
+
+    let walkdir = WalkDir::new(src_dir.to_string());
+    let it = walkdir.into_iter();
+
+    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let _write_zip = write_zip("C:\\Users\\den99\\Desktop\\Work\\DodoRare\\creator\\crates\\creator-tools\\res\\mipmap\\", "C:\\Users\\den99\\Desktop\\Work\\DodoRare\\creator\\crates\\creator-tools\\res\\mipmap\\doit.zip", zip::CompressionMethod::Stored);
+    }
+}
