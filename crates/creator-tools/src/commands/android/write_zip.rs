08c85924f54c629119f7b47bbcc65f2ee2aefd20
OffTheHeezay <den_9912@mail.ru>
2021-08-03 16:59:37 +0300

    Refactor write_zip code

--- crates/creator-tools/src/commands/android/write_zip.rs
+++ crates/creator-tools/src/commands/android/write_zip.rs
@@ -1,65 +1,11 @@
-use std::{
-    fs::File,
-    io::{Read, Seek, Write},
-    iter::Iterator,
-    path::Path,
-};
-use walkdir::{DirEntry, WalkDir};
-use zip::{result::ZipError, write::FileOptions};
+use std::path::{Path, PathBuf};
+use zip::ZipWriter;
+use zip_extensions::write::ZipWriterExtensions;
 
-fn zip_dir<T>(
-    it: &mut dyn Iterator<Item = DirEntry>,
-    prefix: &str,
-    writer: T,
-    method: zip::CompressionMethod,
-) -> zip::result::ZipResult<()>
-where
-    T: Write + Seek,
-{
-    let mut zip = zip::ZipWriter::new(writer);
-    let options = FileOptions::default().compression_method(method);
-
-    let mut buffer = Vec::new();
-    for entry in it {
-        let path = entry.path();
-        let name = path.strip_prefix(Path::new(prefix)).unwrap();
-
-        if path.is_file() {
-            println!("adding file {:?} as {:?} ...", path, name);
-            #[allow(deprecated)]
-            zip.start_file_from_path(name, options)?;
-            let mut f = File::open(path)?;
-
-            f.read_to_end(&mut buffer)?;
-            zip.write_all(&buffer)?;
-            buffer.clear();
-        } else if name.as_os_str().len() != 0 {
-            println!("adding dir {:?} as {:?} ...", path, name);
-            #[allow(deprecated)]
-            zip.add_directory_from_path(name, options)?;
-        }
-    }
-    zip.finish()?;
-    Result::Ok(())
-}
-
-fn write_zip(
-    src_dir: &str,
-    dst_file: &str,
-    method: zip::CompressionMethod,
-) -> zip::result::ZipResult<()> {
-    if !Path::new(src_dir).is_dir() {
-        return Err(ZipError::FileNotFound);
-    }
-
-    let path = Path::new(dst_file);
-    let file = File::create(&path).unwrap();
-
-    let walkdir = WalkDir::new(src_dir.to_string());
-    let it = walkdir.into_iter();
-
-    zip_dir(&mut it.filter_map(|e| e.ok()), src_dir, file, method)?;
-
+fn write(source_path: &PathBuf, archive_file: &Path) -> zip::result::ZipResult<()> {
+    let file = std::fs::File::create(archive_file)?;
+    let mut zip = ZipWriter::new(file);
+    zip.create_from_directory(&source_path)?;
     Ok(())
 }
 
@@ -69,6 +15,6 @@ mod tests {
 
     #[test]
     fn test() {
-        let _write_zip = write_zip("C:\\Users\\den99\\Desktop\\Work\\DodoRare\\creator\\crates\\creator-tools\\res\\mipmap\\", "C:\\Users\\den99\\Desktop\\Work\\DodoRare\\creator\\crates\\creator-tools\\res\\mipmap\\doit.zip", zip::CompressionMethod::Stored);
+        let _write_zip = write(&Path::new("C:\\Users\\den99\\Desktop\\Work\\DodoRare\\creator\\crates\\creator-tools\\res\\mipmap\\").to_owned(), Path::new("C:\\Users\\den99\\Desktop\\Work\\DodoRare\\creator\\crates\\creator-tools\\res\\mipmap\\doit.zip"));
     }
 }
