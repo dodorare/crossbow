e6b050eb4d723d7a7280b8346823d14d2b2a7748
Daniil Anikin <81761954+Heezay@users.noreply.github.com>
2022-01-19 19:21:08 +0300

    Update add_libs_into_aapt2.rs

--- crossbundle/tools/src/commands/android/add_libs_into_aapt2.rs
+++ crossbundle/tools/src/commands/android/add_libs_into_aapt2.rs
@@ -51,7 +51,7 @@ pub fn add_libs_into_aapt2(
     Ok(out_dir)
 }
 
-/// Copy lib into dir then add this lib into apk file
+/// Copy lib into `out_dir` then add this lib into apk file
 pub fn add_lib_aapt2(lib_path: &Path, out_dir: &Path) -> Result<()> {
     if !lib_path.exists() {
         return Err(Error::PathNotFound(lib_path.to_owned()));
