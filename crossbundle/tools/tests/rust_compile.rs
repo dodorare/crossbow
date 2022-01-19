50096a57fc3f842867bd0d1be56871706f3817a6
OffTheHeezay <den_9912@mail.ru>
2022-01-19 17:54:52 +0300

    Fix android full and rust compile tests

--- crossbundle/tools/tests/rust_compile.rs
+++ crossbundle/tools/tests/rust_compile.rs
@@ -1,48 +1,56 @@
-// #[cfg(target_os = "macos")]
-// use crossbundle_tools::commands::apple::*;
-// use crossbundle_tools::{
-//     commands::{android::*, gen_minimal_project},
-//     tools::{AndroidNdk, AndroidSdk},
-//     types::*,
-// };
+#[cfg(target_os = "macos")]
+use crossbundle_tools::commands::apple::*;
+use crossbundle_tools::{
+    commands::{android, gen_minimal_mq_project},
+    tools::{AndroidNdk, AndroidSdk},
+    types::*,
+};
 
-// #[test]
-// fn test_compile_android() {
-//     let tempdir = tempfile::tempdir().unwrap();
-//     let dir = tempdir.path();
-//     let _name = gen_minimal_project(dir).unwrap();
+#[test]
+fn test_compile_android() {
+    // Creates temporary directory
+    let tempdir = tempfile::tempdir().unwrap();
+    let dir = tempdir.path();
+    let package_name = gen_minimal_mq_project(&dir).unwrap();
 
-//     let sdk = AndroidSdk::from_env().unwrap();
-//     let ndk = AndroidNdk::from_env(Some(sdk.sdk_path())).unwrap();
-//     compile_rust_for_android(
-//         &ndk,
-//         Target::Lib,
-//         AndroidTarget::Aarch64LinuxAndroid,
-//         dir,
-//         Profile::Release,
-//         vec![],
-//         false,
-//         false,
-//         30,
-//     )
-//     .unwrap();
-// }
+    // Create dependencies
+    let sdk = AndroidSdk::from_env().unwrap();
+    let ndk = AndroidNdk::from_env(Some(sdk.sdk_path())).unwrap();
+    let target_sdk_version = 30;
+    let profile = Profile::Release;
+    let build_target = AndroidTarget::Aarch64LinuxAndroid;
+    let lib_name = format!("lib{}.so", package_name.replace("-", "_"));
 
-// #[test]
-// #[cfg(target_os = "macos")]
-// fn test_compile_apple() {
-//     let tempdir = tempfile::tempdir().unwrap();
-//     let dir = tempdir.path();
-//     let name = gen_minimal_project(dir).unwrap();
+    // Compile rust code for android with macroquad engine
+    android::compile_rust_for_android_with_mq(
+        &ndk,
+        build_target,
+        &dir,
+        profile,
+        vec![],
+        false,
+        false,
+        target_sdk_version,
+        &lib_name,
+    )
+    .unwrap();
+}
 
-//     compile_rust_for_ios(
-//         Target::Bin(name),
-//         AppleTarget::Aarch64AppleIos,
-//         dir,
-//         Profile::Release,
-//         vec![],
-//         false,
-//         false,
-//     )
-//     .unwrap();
-// }
+#[test]
+#[cfg(target_os = "macos")]
+fn test_compile_apple() {
+    let tempdir = tempfile::tempdir().unwrap();
+    let dir = tempdir.path();
+    let name = gen_minimal_project(dir).unwrap();
+
+    compile_rust_for_ios(
+        Target::Bin(name),
+        AppleTarget::Aarch64AppleIos,
+        dir,
+        Profile::Release,
+        vec![],
+        false,
+        false,
+    )
+    .unwrap();
+}
