1de30e7f6581ccee8aa4b5feb790d20cf39c94a3
OffTheHeezay <den_9912@mail.ru>
2022-04-05 18:30:56 +0300

    Fix android compilation for macroquad

--- examples/macroquad-ui/src/main.rs
+++ examples/macroquad-ui/src/main.rs
@@ -1,45 +1,45 @@
 use macroquad::prelude::*;
 use macroquad::ui::{hash, root_ui, Skin};
 
-#[macroquad::main("UI showcase")]
-async fn main() {
+#[macroquad::main("Macroquad UI")]
+async fn main() -> anyhow::Result<()> {
     let skin = {
         let label_style = root_ui()
             .style_builder()
-            .font(include_bytes!("ui_assets/HTOWERT.TTF"))
-            .unwrap()
+            // .font()
+            // .unwrap()
             .text_color(Color::from_rgba(180, 180, 120, 255))
             .font_size(30)
             .build();
 
         let window_style = root_ui()
             .style_builder()
-            .background(Image::from_file_with_format(
-                include_bytes!("ui_assets/window_background.png"),
-                None,
-            ))
+            // .background(Image::from_file_with_format(
+            //     include_bytes!("ui_assets/window_background.png"),
+            //     None,
+            // ))
             .background_margin(RectOffset::new(20.0, 20.0, 10.0, 10.0))
             .margin(RectOffset::new(-20.0, -30.0, 0.0, 0.0))
             .build();
 
         let button_style = root_ui()
             .style_builder()
-            .background(Image::from_file_with_format(
-                include_bytes!("ui_assets/button_background.png"),
-                None,
-            ))
+            // .background(Image::from_file_with_format(
+            //     include_bytes!("ui_assets/button_background.png"),
+            //     None,
+            // ))
             .background_margin(RectOffset::new(37.0, 37.0, 5.0, 5.0))
             .margin(RectOffset::new(10.0, 10.0, 0.0, 0.0))
-            .background_hovered(Image::from_file_with_format(
-                include_bytes!("ui_assets/button_hovered_background.png"),
-                None,
-            ))
-            .background_clicked(Image::from_file_with_format(
-                include_bytes!("ui_assets/button_clicked_background.png"),
-                None,
-            ))
-            .font(include_bytes!("ui_assets/HTOWERT.TTF"))
-            .unwrap()
+            // .background_hovered(Image::from_file_with_format(
+            //     include_bytes!("ui_assets/button_hovered_background.png"),
+            //     None,
+            // ))
+            // .background_clicked(Image::from_file_with_format(
+            //     include_bytes!("ui_assets/button_clicked_background.png"),
+            //     None,
+            // ))
+            // .font(include_bytes!("ui_assets/HTOWERT.TTF"))
+            // .unwrap()
             .text_color(Color::from_rgba(180, 180, 100, 255))
             .font_size(40)
             .build();
@@ -47,8 +47,8 @@ async fn main() {
         let editbox_style = root_ui()
             .style_builder()
             .background_margin(RectOffset::new(0., 0., 0., 0.))
-            .font(include_bytes!("ui_assets/HTOWERT.TTF"))
-            .unwrap()
+            // .font(include_bytes!("ui_assets/HTOWERT.TTF"))
+            // .unwrap()
             .text_color(Color::from_rgba(120, 120, 120, 255))
             .color_selected(Color::from_rgba(190, 190, 190, 255))
             .font_size(50)
@@ -64,7 +64,6 @@ async fn main() {
     };
 
     let mut window_skin = skin.clone();
-    // let action_font = load_ttf_font("ui_assets/HTOWERT.TTF").await.unwrap();
     loop {
         clear_background(BROWN);
 
