0bdf33a88ac4bf8dd5a4627f341921fe252c0061
unknown <den_9912@mail.ru>
2022-07-26 19:30:21 +0500

    Fix linux build

--- crossbundle/tools/src/tools/android_ndk.rs
+++ crossbundle/tools/src/tools/android_ndk.rs
@@ -324,7 +324,7 @@ impl AndroidNdk {
 pub fn ndk_install_path() -> crate::error::Result<String> {
     let ndk_path = PathBuf::from(android_tools::sdk_install_path()?).join("ndk");
     #[cfg(target_os = "linux")]
-    let ndk_path = PathBuf::from(android_tools::sdk_install_path()?).join("ndk");
+    let ndk_path = PathBuf::from(android_tools::sdk_install_path()?).join("ndk-bundle");
     let ndk_ver = std::fs::read_dir(&ndk_path)
         .map_err(|_| Error::PathNotFound(ndk_path.clone()))?
         .filter_map(|path| path.ok())
