ba70ef9f029457a802d09ead83f11568fa8e5d9f
David Ackerman <enfipy@gmail.com>
2022-10-10 23:00:42 +0200

    Fix clippy

--- crossbundle/tools/src/commands/android/common/detect_abi.rs
+++ crossbundle/tools/src/commands/android/common/detect_abi.rs
@@ -9,6 +9,6 @@ pub fn detect_abi(sdk: &AndroidSdk) -> Result<AndroidTarget> {
     let mut adb = sdk.platform_tool(bin!("adb"))?;
     adb.arg("shell").arg("getprop").arg("ro.product.cpu.abi");
     let stdout = adb.output_err(true)?.stdout;
-    let abi = std::str::from_utf8(&stdout).or(Err(AndroidError::UnsupportedTarget))?;
+    let abi = std::str::from_utf8(&stdout).map_err(|_| AndroidError::UnsupportedTarget)?;
     AndroidTarget::from_android_abi(abi.trim())
 }
